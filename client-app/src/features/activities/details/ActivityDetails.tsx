d0a60e042a0fb81c3842cd09eba801e68769ce90
Augha <fabioes0207@gmail.com>
2024-01-10 13:41:44 +0000

    Struct and add CRUD Operation to frontend
    Struct and add CRUD Operation to frontend

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,53 @@
+import {
+	Button,
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardMeta,
+	Image,
+} from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+	activity: Activity;
+	cancelActivity: () => void;
+	openForm: (id: string) => void;
+}
+
+export default function ActivityDetails({
+	activity,
+	cancelActivity,
+	openForm,
+}: Props) {
+	return (
+		<Card fluid>
+			<Image
+				src={`../../../../public/assets/categoryImages/${activity.category}.jpg`}
+			/>
+			<CardContent>
+				<CardHeader>{activity.title}</CardHeader>
+				<CardMeta>
+					<span>{activity.date}</span>
+				</CardMeta>
+				<CardDescription>{activity.description}</CardDescription>
+			</CardContent>
+			<CardContent extra>
+				<Button.Group widths="2">
+					<Button
+						basic
+						color="blue"
+						content="Edit"
+						onClick={() => openForm(activity.id)}
+					/>
+					<Button
+						basic
+						color="grey"
+						content="Cancel"
+						onClick={() => cancelActivity()}
+					/>
+				</Button.Group>
+			</CardContent>
+		</Card>
+	);
+}
